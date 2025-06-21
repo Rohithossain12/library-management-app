b1e8aa86d7ff8455ee6566d35ff5c5b78cd2c5a9
Rohithossain12 <https://github.com/Rohithossain12>
2025-06-22 01:04:49 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import { Server } from "http";
+import mongoose from "mongoose";
+import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+let server: Server;
+const PORT = 5000;
+
+async function main() {
+
+    try {
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uv360.mongodb.net/library-management-app?retryWrites=true&w=majority&appName=Cluster0`);
+        console.log("Connected to mongoDB Using Mongoose");
+        server = app.listen(PORT, () => {
+            console.log(`App is Listening on port ${PORT}`);
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+main()
