08026660f4c6c531010e72a24d4e5d1315af7fc3
Rohithossain12 <https://github.com/Rohithossain12>
2025-06-24 23:01:01 +0600

    tsc command run

--- dist/app.js
+++ dist/app.js
@@ -12,6 +12,9 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use("/api/books", book_controller_1.booksRouter);
 app.use("/api/borrow", borrow_controller_1.default);
+app.get("/", (req, res) => {
+    res.send('Welcome to Library Management app');
+});
 // 404 Not Found Handler
 app.use((req, res) => {
     res.status(404).json({
@@ -25,7 +28,4 @@ app.use((req, res) => {
 });
 // Global Error Handler
 app.use(globalErrorHandler_1.globalErrorHandler);
-app.get("/", (req, res) => {
-    res.send('Welcome to Library Management app');
-});
 exports.default = app;
